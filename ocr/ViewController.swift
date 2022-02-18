53ff0bb4f60063bd81a14678eed54eff43ef8c11
sangyeon3 <sangyeun3@naver.com>
2022-02-18 15:55:20 +0900

    Initial Commit

---
+++ ocr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ocr
+//
+//  Created by  sangyeon on 2022/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
